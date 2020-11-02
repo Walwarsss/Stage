b0f57919684ec1d7661980da6a14b6c0cf32a8ad
Walwarsss <geoffrey.lazzari@epitech.eu>
2020-11-02 10:13:02 +0100

    lib

---
+++ Code/lib/Batflow/Batflow.cpp
@@ -0,0 +1,16 @@
+#include "Batflow.h"
+
+// PUBLIC
+
+Batflow Batflow::read() {
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2);
+  ADCSRA |= _BV(ADSC);
+  while (bit_is_set(ADCSRA, ADSC));
+  _val = 1126400L / (ADCL | (ADCH << 8));
+  _percent = (_val * 100) / (float)max(_val, high);
+  _level = max(0, map(_val, min(_val, low), max(_val, high), 0, full));
+  return *this;
+}
+
+// PRIVATE
