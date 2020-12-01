72ef42cb4e531667c0fb2f4682b829a33208606d
Walwarsss <geoffrey.lazzari@epitech.eu>
2020-12-01 13:33:48 +0100

    code de test

--- VMC/src/Code_de_test/test_vibration_atom.ino
+++ VMC/src/Code_de_test/test_vibration_atom.ino
@@ -1,10 +1,12 @@
+#include <M5Atom.h> 
 #include <Arduino.h> 
-#include <M5StickC.h>
 
 // Propriétés Valeur Accéléromètre
 float X = 0;
 float Y = 0;
 float Z = 0;
+
+int is_vmc_on = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -20,12 +22,17 @@ void loop() {
       delay(100); // att 0.1sec pour l'échantillonnage des valuers
       M5.IMU.getAccelData(&X,&Y,&Z);
       Y = (Y < 0) ?-Y : Y;
+      Serial.printf("Y : %f\n", Y);
       res += Y;
       i++;
   }
   res /= i;
+  Serial.printf("res : %f\n", res);
+  delay(5000);
   is_vmc_on = (res > 0.030) ? 1 : 0;
   if (is_vmc_on == 1) {
     M5.dis.drawpix(0, 0xf00000);
+    delay(1000);
+    M5.update();
     }
 }
